785c58486e1d8a95ad1a329659b3b351897f2a8a
tonyqtian <tonyqtian@gmail.com>
2017-02-21 19:42:54 +0800

    changed optimizer parameters to default

--- nea/optimizers.py
+++ nea/optimizers.py
@@ -7,7 +7,8 @@ def get_optimizer(args):
 	clipnorm = 0
 
 	if args.algorithm == 'rmsprop':
-		optimizer = opt.RMSprop(lr=0.001, rho=0.9, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
+# 		optimizer = opt.RMSprop(lr=0.001, rho=0.9, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
+		optimizer = opt.RMSprop(lr=0.01)
 	elif args.algorithm == 'sgd':
 		optimizer = opt.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False, clipnorm=clipnorm, clipvalue=clipvalue)
 	elif args.algorithm == 'adagrad':
@@ -15,7 +16,8 @@ def get_optimizer(args):
 	elif args.algorithm == 'adadelta':
 		optimizer = opt.Adadelta(lr=1.0, rho=0.95, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
 	elif args.algorithm == 'adam':
-		optimizer = opt.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
+# 		optimizer = opt.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
+		optimizer = opt.Adam()
 	elif args.algorithm == 'adamax':
 		optimizer = opt.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
 	
