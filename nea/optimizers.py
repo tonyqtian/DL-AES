d6d1209ebbd429c62f06dd93ff03acd76df1773a
tonyqtian <tonyqtian@gmail.com>
2017-02-24 19:53:48 +0800

    updated optimizer learning rate setting

--- nea/optimizers.py
+++ nea/optimizers.py
@@ -9,21 +9,39 @@ def get_optimizer(args):
 	if args.algorithm == 'rmsprop':
 # 		optimizer = opt.RMSprop(lr=0.001, rho=0.9, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
 # 		optimizer = opt.RMSprop(lr=0.01)
-		optimizer = opt.RMSprop()
+		if args.learning_rate > 0:
+			optimizer = opt.RMSprop(lr=args.learning_rate)
+		else:
+			optimizer = opt.RMSprop()
 	elif args.algorithm == 'sgd':
 # 		optimizer = opt.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False, clipnorm=clipnorm, clipvalue=clipvalue)
-		optimizer = opt.SGD()
+		if args.learning_rate > 0:
+			optimizer = opt.SGD(lr=args.learning_rate)
+		else:
+			optimizer = opt.SGD()
 	elif args.algorithm == 'adagrad':
 # 		optimizer = opt.Adagrad(lr=0.01, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
-		optimizer = opt.Adagrad()
+		if args.learning_rate > 0:
+			optimizer = opt.Adagrad(lr=args.learning_rate)
+		else:
+			optimizer = opt.Adagrad()
 	elif args.algorithm == 'adadelta':
 # 		optimizer = opt.Adadelta(lr=1.0, rho=0.95, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
-		optimizer = opt.Adadelta()
+		if args.learning_rate > 0:
+			optimizer = opt.Adadelta(lr=args.learning_rate)
+		else:
+			optimizer = opt.Adadelta()
 	elif args.algorithm == 'adam':
 # 		optimizer = opt.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
-		optimizer = opt.Adam()
+		if args.learning_rate > 0:
+			optimizer = opt.Adam(lr=args.learning_rate)
+		else:
+			optimizer = opt.Adam()
 	elif args.algorithm == 'adamax':
 # 		optimizer = opt.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
-		optimizer = opt.Adamax()
+		if args.learning_rate > 0:
+			optimizer = opt.Adamax(lr=args.learning_rate)
+		else:
+			optimizer = opt.Adamax()
 	
 	return optimizer
