3dcc6ae53c77aeff695f97d7c8c1bca2eaddca6d
tonyqtian <tonyqtian@gmail.com>
2017-02-24 19:42:33 +0800

    set optimizers prama back to default

--- nea/optimizers.py
+++ nea/optimizers.py
@@ -2,23 +2,28 @@ import keras.optimizers as opt
 
 def get_optimizer(args):
 
-	clipvalue = 0
+# 	clipvalue = 0
 # 	clipnorm = 10
-	clipnorm = 0
+# 	clipnorm = 0
 
 	if args.algorithm == 'rmsprop':
 # 		optimizer = opt.RMSprop(lr=0.001, rho=0.9, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
-		optimizer = opt.RMSprop(lr=0.01)
+# 		optimizer = opt.RMSprop(lr=0.01)
+		optimizer = opt.RMSprop()
 	elif args.algorithm == 'sgd':
-		optimizer = opt.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False, clipnorm=clipnorm, clipvalue=clipvalue)
+# 		optimizer = opt.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False, clipnorm=clipnorm, clipvalue=clipvalue)
+		optimizer = opt.SGD()
 	elif args.algorithm == 'adagrad':
-		optimizer = opt.Adagrad(lr=0.01, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
+# 		optimizer = opt.Adagrad(lr=0.01, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
+		optimizer = opt.Adagrad()
 	elif args.algorithm == 'adadelta':
-		optimizer = opt.Adadelta(lr=1.0, rho=0.95, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
+# 		optimizer = opt.Adadelta(lr=1.0, rho=0.95, epsilon=1e-06, clipnorm=clipnorm, clipvalue=clipvalue)
+		optimizer = opt.Adadelta()
 	elif args.algorithm == 'adam':
 # 		optimizer = opt.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
 		optimizer = opt.Adam()
 	elif args.algorithm == 'adamax':
-		optimizer = opt.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
+# 		optimizer = opt.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=clipnorm, clipvalue=clipvalue)
+		optimizer = opt.Adamax()
 	
 	return optimizer
